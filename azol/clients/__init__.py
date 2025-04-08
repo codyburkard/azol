e7535337f99acdf7c2db3e9923e922c654ab61b4
codyburkard <me@codyburkard.com>
2025-04-08 18:54:08 +0200

    adding kudu client
    Signed-off-by: codyburkard <me@codyburkard.com>

--- azol/clients/__init__.py
+++ azol/clients/__init__.py
@@ -4,11 +4,13 @@ from azol.clients.graph_client import GraphClient
 from azol.clients.key_vault_client import KeyVaultClient
 from azol.clients.oauth_http_client import OAuthHTTPClient
 from azol.clients.datafactory_client import DataFactoryClient
+from azol.clients.kudu_client import KuduClient
 
 __all__ = [
     "ArmClient",
     "GraphClient",
     "KeyVaultClient",
     "OAuthHTTPClient",
-    "DataFactoryClient"
+    "DataFactoryClient",
+    "KuduClient"
 ]
